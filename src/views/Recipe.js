d11c66b2f55d5f304b0ff62cc4f05f8f42f3bf8d
C836 <gl.sepiol@gmail.com>
2022-08-18 12:00:04 -0300

    chore: atualizando componentes do módulo de receitas

--- src/views/Recipe.js
+++ src/views/Recipe.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { Details } from "../components";
+import { Background } from "../components/Background";
 import { getRecipe } from "../services/recipe";
 
 export function Recipe() {
   const [recipe, setRecipe] = useState();
 
   const recipeRequest = () => {
-    getRecipe().then((response) => setRecipe(response.recipe));
+    getRecipe().then((response) => setRecipe(response));
   };
 
   useEffect(recipeRequest, []);
+
+  if (recipe?.image)
+    return (
+      <>
+        <Background
+          image={recipe.image} />
+        <Details
+          title={recipe.title}
+          summary={recipe.summary}
+          prepareTime={recipe.readyInMinutes}
+          servings={recipe.servings}
+        />
+      </>
+    );
 }
