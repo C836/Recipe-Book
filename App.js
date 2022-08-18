f360ba277ba26699b279b712533f782e879b1eff
C836 <gl.sepiol@gmail.com>
2022-08-18 12:01:44 -0300

    chore: impotando módulo de receitas no módulo principal

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
-export default function App() {}
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import { Recipe } from "./src/views/Recipe";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Recipe />
+    </SafeAreaView>
+  );
+}
 
 export const theme = {
   dark: false,
