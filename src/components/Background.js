1a4e23dd3cd59430ab6bebb4e6bab5a73bc1037c
C836 <gl.sepiol@gmail.com>
2022-08-19 14:15:54 -0300

    chore: background estático

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { StyleSheet, Image, Dimensions } from "react-native";
-
-const { width } = Dimensions.get("screen");
-
-import { getImageDimensions } from "./../utils/getImageDimensions";
+import { StyleSheet, Image } from "react-native";
 
 export function Background({ image }) {
-  return (
-    <Image
-      style={styles(getImageDimensions(image)).background}
-      source={{ uri: image }}
-    />
-  );
+  return <Image style={styles.background} source={{ uri: image }} />;
 }
 
-const styles = ({ imgWidth, imgHeight }) =>
-  StyleSheet.create({
-    background: {
-      width: "100%",
-      height: (imgHeight / imgWidth) * width,
-    },
-  });
+const styles = StyleSheet.create({
+  background: {
+    position: "absolute",
+    width: "100%",
+    height: 225,
+  },
+});
