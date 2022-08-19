594aeda0b5f69bf8cf203c03dbe4b1df2693d66d
C836 <gl.sepiol@gmail.com>
2022-08-19 14:05:16 -0300

    chore: atualizando componente Ingredient
    destacando a quantidade

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,14 +1,24 @@
-import { Roboto_100Thin } from "@expo-google-fonts/roboto";
 import React from "react";
 
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Paragraph } from "./Paragraph";
 
-export function Ingredient({ index, children }) {
+export function Ingredient({ children }) {
+  const getAmount = (text) => {
+    return text.split(" ")[0];
+  };
+
+  const getIngredient = (text) => {
+    return text.replace(getAmount(children), "");
+  };
+
   return (
     <View style={style.ingredient}>
       <View style={style.dot} />
-      <Paragraph style={{ width: "96%" }}>{children}</Paragraph>
+      <Paragraph style={{ width: "96%" }}>
+        <Text style={{ fontWeight: "bold" }}>{getAmount(children)}</Text>
+        {getIngredient(children)}
+      </Paragraph>
     </View>
   );
 }
@@ -16,8 +26,8 @@ export function Ingredient({ index, children }) {
 const style = StyleSheet.create({
   dot: {
     backgroundColor: "red",
-    width: 8,
-    height: 8,
+    width: 6,
+    height: 6,
     borderRadius: 50,
     marginRight: 10,
   },
