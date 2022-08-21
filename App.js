aa8c644ca605e2da53b7a6837d795d8f7e09af97
C836 <gl.sepiol@gmail.com>
2022-08-21 03:16:58 -0300

    chore: integrando página inicial ao módulo da aplicação

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 import { Recipe } from "./src/views/Recipe";
+import { Main } from "./src/views/Main";
 
 export default function App() {
+  const [recipeData, setRecipe] = useState({
+    id: null,
+    data: null,
+  });
+
+  useEffect(() => {
+    console.log(recipeData);
+  }, [recipeData]);
+
   return (
     <SafeAreaView>
       <StatusBar />
       <Recipe />
+      <Main 
+        recipedata={recipeData} 
+        setRecipe={setRecipe} />
     </SafeAreaView>
   );
 }
